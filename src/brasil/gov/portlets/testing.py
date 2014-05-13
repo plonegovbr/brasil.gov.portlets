0b2490f3a705fbb490d9ac74b43aa2490a554b5b
Rodrigo Ferreira de Souza <rodfersou@gmail.com>
2014-05-12 21:46:23 -0300

    Adicionando mais recursos para os testes (imagens, arquivos e noticia com imagem)

--- src/brasil/gov/portlets/testing.py
+++ src/brasil/gov/portlets/testing.py
@@ -1,11 +1,60 @@
 # -*- coding: utf-8 -*-
 
+from App.Common import package_home
+from PIL import Image
+from plone import api
 from plone.app.robotframework.testing import AUTOLOGIN_LIBRARY_FIXTURE
 from plone.app.testing import FunctionalTesting
 from plone.app.testing import IntegrationTesting
 from plone.app.testing import PLONE_FIXTURE
 from plone.app.testing import PloneSandboxLayer
 from plone.testing import z2
+from StringIO import StringIO
+
+import os
+import random
+
+
+def loadFile(name, size=0):
+    """Load file from testing directory
+    """
+    path = os.path.join(package_home(globals()), 'tests/input', name)
+    fd = open(path, 'rb')
+    data = fd.read()
+    fd.close()
+    return data
+
+
+def generate_jpeg(width, height):
+    # Mandelbrot fractal
+    # FB - 201003254
+    # drawing area
+    xa = -2.0
+    xb = 1.0
+    ya = -1.5
+    yb = 1.5
+    maxIt = 25  # max iterations allowed
+    # image size
+    image = Image.new('RGB', (width, height))
+    c = complex(random.random() * 2.0 - 1.0, random.random() - 0.5)
+
+    for y in range(height):
+        zy = y * (yb - ya) / (height - 1) + ya
+        for x in range(width):
+            zx = x * (xb - xa) / (width - 1) + xa
+            z = complex(zx, zy)
+            for i in range(maxIt):
+                if abs(z) > 2.0:
+                    break
+                z = z * z + c
+            r = i % 4 * 64
+            g = i % 8 * 32
+            b = i % 16 * 16
+            image.putpixel((x, y), b * 65536 + g * 256 + r)
+
+    output = StringIO()
+    image.save(output, format='PNG')
+    return output.getvalue()
 
 
 class Fixture(PloneSandboxLayer):
@@ -19,6 +68,33 @@ class Fixture(PloneSandboxLayer):
 
     def setUpPloneSite(self, portal):
         self.applyProfile(portal, 'brasil.gov.portlets:default')
+        with api.env.adopt_roles(['Manager']):
+            obj = api.content.create(
+                type='Image',
+                title='My Image',
+                container=portal)
+            obj.setImage(generate_jpeg(50, 50))
+            obj = api.content.create(
+                type='Image',
+                title='My Image1',
+                container=portal)
+            obj.setImage(generate_jpeg(50, 50))
+            obj = api.content.create(
+                type='Image',
+                title='My Image2',
+                container=portal)
+            obj.setImage(generate_jpeg(50, 50))
+            obj = api.content.create(
+                type='File',
+                title='My File',
+                container=portal)
+            obj.setFile(loadFile('lorem_ipsum.txt'))
+            obj.reindexObject()
+            obj = api.content.create(
+                type='News Item',
+                title='My News Item',
+                container=portal)
+            obj.setImage(generate_jpeg(50, 50))
         portal.portal_workflow.setDefaultChain('simple_publication_workflow')
 
 
