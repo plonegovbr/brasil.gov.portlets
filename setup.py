b164c5f41d0c5902906961c8740f9d15dda14387
Héctor Velarde <hector.velarde@gmail.com>
2014-12-05 17:40:38 -0200

    Preparing release 1.0b1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.0.1dev'
+version = '1.0b1'
 description = 'Portlets para Portal Padrão'
 long_description = (
     open('README.rst').read() + '\n' +
